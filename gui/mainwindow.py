579d9df38f08ca249dad9f885daaea55ed08832e
Wouter Wijsman <wwijsman@live.nl>
2019-01-03 01:44:16 +0100

    Updated mainwindow.py to work with the application,glade file

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -4,58 +4,35 @@ from gi.repository import Gtk
 from sys import platform
 if not platform.startswith('darwin'):
     from gui.loginwindow import LoginWindow
-else:
-    import webbrowser
 
 
-class MainWindow(Gtk.Window):
+@Gtk.Template.from_file("gui/glade/application.glade")
+class MainWindow(Gtk.ApplicationWindow):
+
+    __gtype_name__ = "Window"
+
+    header_sync = Gtk.Template.Child()
+    menu_about = Gtk.Template.Child()
+    menu_preferences = Gtk.Template.Child()
+    menu_logout = Gtk.Template.Child()
+    library = Gtk.Template.Child()
+
+    api = None
 
     def __init__(self, name, api):
+        Gtk.ApplicationWindow.__init__(self, title=name)
         self.api = api
-        Gtk.Window.__init__(self, title=name)
-
-        # Load the content of the window from a glade file
-
-        builder = Gtk.Builder()
-        builder.add_from_file("gui/glade/mainwindow.glade")
-        builder.connect_signals(self)
-
-        vbox = builder.get_object("vbox")
-
-        self.library = builder.get_object("window_library")
-
-
-        #self.button_box = Gtk.Box()
-
-
-        #self.library_button = Gtk.Button.new_with_label("library")
-        #self.library_button.connect("clicked", self.on_button_get_library_clicked)
-        #self.button_box.pack_start(self.library_button, True, True, 0)
-
-        self.add(vbox)
-        self.show_all()
-
-    def on_button_login_clicked(self, button):
-        if not platform.startswith('darwin'):
-            LoginWindow(self.api)
-        else:
-            webbrowser.open(self.api.get_login_url(), new=1)
-            self.present()
-
-    def on_button_refresh_token_clicked(self, button):
-        print("token needs to be refreshed")
         self.api.read_refresh_token()
         self.api.refresh()
+        self.show_all()
 
-    def on_button_get_library_clicked(self, button):
+    @Gtk.Template.Callback("on_header_sync_clicked")
+    def sync_library(self, button):
         print("go get the library")
         self.api.read_activate_token()
         games = self.api.get_library()
-        game_number = 0
         for product in games['products']:
-            game_number += 1
-            print("{}: {}".format(game_number, product['title']))
+            print(product)
             button = Gtk.Button.new_with_label(product['title'])
             self.library.add(button)
         self.show_all()
-
