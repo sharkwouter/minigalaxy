1feae54e921774822373d0f956089768866ad718
Wouter Wijsman <wwijsman@live.nl>
2020-01-02 02:34:06 +0100

    Renamed generate-pot.sh and added main binary to translations

--- scripts/update-translation-files.sh
+++ scripts/update-translation-files.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"/..
 POTFILE="data/po/minigalaxy.pot"
 
 # Generate the pot file
-xgettext --from-code=UTF-8 --keyword=translatable --keyword=_ --sort-output data/ui/*.ui minigalaxy/window/*.py -o "${POTFILE}"
+xgettext --from-code=UTF-8 --keyword=translatable --keyword=_ --sort-output data/ui/*.ui minigalaxy/window/*.py bin/minigalaxy -o "${POTFILE}"
 
 # Update each po file
 for langfile in data/po/*.po; do
