56923f9505e0912b5e94f8e4238dab6e2cce5141
Wouter Wijsman <wwijsman@live.nl>
2020-01-02 02:09:02 +0100

    Changed generate-pot.sh script to update po files

--- scripts/generate-pot.sh
+++ scripts/generate-pot.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 cd "$(dirname "$0")"/..
-xgettext --from-code=UTF-8 --keyword=translatable --keyword=_ --sort-output data/ui/*.ui minigalaxy/window/*.py -o data/po/minigalaxy.pot
+
+POTFILE="data/po/minigalaxy.pot"
+
+# Generate the pot file
+xgettext --from-code=UTF-8 --keyword=translatable --keyword=_ --sort-output data/ui/*.ui minigalaxy/window/*.py -o "${POTFILE}"
+
+# Update each po file
+for langfile in data/po/*.po; do
+	msgmerge -U "${langfile}" "${POTFILE}"
+done
