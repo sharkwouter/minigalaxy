281c5c65db8087bf7fa7b7a9c31b34c8a4ee8792
Wouter Wijsman <wwijsman@live.nl>
2020-02-09 20:59:56 +0100

    Added debug mode which makes the API show request output Just set the MG_DEBUG environment variable to anything

--- minigalaxy/api.py
+++ minigalaxy/api.py
@@ -1,3 +1,4 @@
+import os
 import time
 from urllib.parse import urlencode
 import requests
@@ -12,6 +13,7 @@ class Api:
         self.redirect_uri = "https://embed.gog.com/on_login_success?origin=client"
         self.client_id = "46899977096215655"
         self.client_secret = "9d85c43b1482497dbbce61f6e4aa173a433796eeae2ca8c5f6129f2dc4de46d9"
+        self.debug = os.environ.get("MG_DEBUG")
 
     # use a method to authenticate, based on the information we have
     # Returns an empty string if no information was entered
@@ -158,4 +160,8 @@ class Api:
             'Authorization': "Bearer " + self.active_token,
         }
         response = requests.get(url, headers=headers, params=params)
+        if self.debug:
+            print("Request: {}".format(url))
+            print("Response body: {}".format(response.text))
+            print("")
         return response.json()
