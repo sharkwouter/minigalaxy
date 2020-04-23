3c53fba0d44d3577b3842dc288048d1712d77a64
Wouter Wijsman <wwijsman@live.nl>
2020-04-23 20:24:41 +0200

    Prevent crash on systems with incomplete locale support
    Closes #139

--- minigalaxy/translation.py
+++ minigalaxy/translation.py
@@ -5,7 +5,11 @@ from minigalaxy.paths import LOCALE_DIR
 TRANSLATION_DOMAIN = "minigalaxy"
 
 locale.setlocale(locale.LC_ALL, '')
-locale.bindtextdomain(TRANSLATION_DOMAIN, LOCALE_DIR)
+try:
+    locale.bindtextdomain(TRANSLATION_DOMAIN, LOCALE_DIR)
+except AttributeError:
+    print("Couldn't run locale.bindtextdomain. Translations might not work correctly.")
+
 gettext.bindtextdomain(TRANSLATION_DOMAIN, LOCALE_DIR)
 gettext.textdomain(TRANSLATION_DOMAIN)
 _ = gettext.gettext
