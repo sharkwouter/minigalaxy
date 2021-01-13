5bf132f4c9002e42ccb090f5f2f3f3820eab7454
Tomasz Makarewicz <tomasz.makarewicz@protonmail.ch>
2021-01-13 21:43:49 +0100

    Define active_token_expiration_time in Api() class constructor.

--- minigalaxy/api.py
+++ minigalaxy/api.py
@@ -19,6 +19,7 @@ class Api:
         self.client_secret = "9d85c43b1482497dbbce61f6e4aa173a433796eeae2ca8c5f6129f2dc4de46d9"
         self.debug = os.environ.get("MG_DEBUG")
         self.active_token = False
+        self.active_token_expiration_time = time.time()
 
     # use a method to authenticate, based on the information we have
     # Returns an empty string if no information was entered
