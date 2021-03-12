31697a8557dd6ed702d4e5e9d4a34c52050208e0
Wouter Wijsman <wouter.wijsman@qbeyond.de>
2021-03-12 12:02:36 +0100

    Catch CSS errors

--- minigalaxy/css.py
+++ minigalaxy/css.py
@@ -2,5 +2,8 @@ from minigalaxy.ui.gtk import Gtk
 from minigalaxy.paths import CSS_PATH
 
 CSS_PROVIDER = Gtk.CssProvider()
-with open(CSS_PATH) as style:
-    CSS_PROVIDER.load_from_data(style.read().encode('utf-8'))
+try:
+    with open(CSS_PATH) as style:
+        CSS_PROVIDER.load_from_data(style.read().encode('utf-8'))
+except gi.repository.GLib.Error:
+    print("The CSS in {} could not be loaded".format(CSS_PATH))
