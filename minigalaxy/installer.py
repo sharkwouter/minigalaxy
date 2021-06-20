5749a0e5ed5c5ce5a8adf90bbafee0d3a3023d6c
Tomasz Makarewicz <tomasz.makarewicz@protonmail.ch>
2021-06-20 14:20:43 +0200

    Add initial option for innoextract.

--- minigalaxy/installer.py
+++ minigalaxy/installer.py
@@ -2,6 +2,9 @@ import os
 import shutil
 import subprocess
 import hashlib
+import tarfile
+
+from minigalaxy.constants import SESSION, DOWNLOAD_CHUNK_SIZE
 from minigalaxy.translation import _
 from minigalaxy.paths import CACHE_DIR, THUMBNAIL_DIR
 from minigalaxy.config import Config
@@ -107,17 +110,52 @@ def make_tmp_dir(game):
 def extract_installer(game, installer, temp_dir):
     # Extract the installer
     error_message = ""
-    if game.platform == "linux":
+    if game.platform in ["linux"]:
         command = ["unzip", "-qq", installer, "-d", temp_dir]
     else:
-        # Set the prefix for Windows games
-        prefix_dir = os.path.join(game.install_dir, "prefix")
-        if not os.path.exists(prefix_dir):
-            os.makedirs(prefix_dir, mode=0o755)
+        command, error_message = extract_by_innoextract(installer, temp_dir)
+        if error_message:
+            command, error_message = extract_by_wine(game, installer, temp_dir)
+    error_message = execute_extraction(command, temp_dir, installer)
+    if error_message and game.platform in ["windows"]:
+        command, error_message = extract_by_wine(game, installer, temp_dir)
+        error_message = execute_extraction(command, temp_dir, installer)
+    return error_message
 
-        # It's possible to set install dir as argument before installation
-        command = ["env", "WINEPREFIX={}".format(prefix_dir), "wine", installer,
-                   "/dir={}".format(temp_dir), "/VERYSILENT"]
+
+def extract_by_innoextract(installer, temp_dir):
+    err_msg = ""
+    innoextract_ver = "1.9"
+    innoextract_tar = "innoextract-{}-linux.tar.xz".format(innoextract_ver)
+    innoextract_url = "https://constexpr.org/innoextract/files/{}".format(innoextract_tar)
+    download_request = SESSION.get(innoextract_url, stream=True, timeout=30)
+    with open(os.path.join(temp_dir, innoextract_tar), "wb") as save_file:
+        for chunk in download_request.iter_content(chunk_size=DOWNLOAD_CHUNK_SIZE):
+            save_file.write(chunk)
+        save_file.close()
+    tar = tarfile.open(os.path.join(temp_dir, innoextract_tar))
+    tar.extractall(path=temp_dir)
+    tar.close()
+    innoextract_file = os.path.join(temp_dir, "innoextract-{}-linux".format(innoextract_ver), "bin", "amd64",
+                                    "innoextract")
+    os.chmod(innoextract_file, 0o775)
+    cmd = [innoextract_file, installer, "-d", os.path.join(temp_dir, "minigalaxy_game_files")]
+    return cmd, err_msg
+
+
+def extract_by_wine(game, installer, temp_dir):
+    err_msg = ""
+    # Set the prefix for Windows games
+    prefix_dir = os.path.join(game.install_dir, "prefix")
+    if not os.path.exists(prefix_dir):
+        os.makedirs(prefix_dir, mode=0o755)
+    # It's possible to set install dir as argument before installation
+    command = ["env", "WINEPREFIX={}".format(prefix_dir), "wine", installer, "/dir={}".format(temp_dir), "/VERYSILENT"]
+    return command, err_msg
+
+
+def execute_extraction(command, temp_dir, installer):
+    err_msg = ""
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     process.wait()
     stdout, stderr = process.communicate()
@@ -125,10 +163,10 @@ def extract_installer(game, installer, temp_dir):
     stderr = stderr.decode("utf-8")
     if (process.returncode not in [0, 1]) or \
        (process.returncode in [1] and "(attempting to process anyway)" not in stderr):
-        error_message = _("The installation of {} failed. Please try again.").format(installer)
+        err_msg = _("The installation of {} failed. Please try again.").format(installer)
     elif len(os.listdir(temp_dir)) == 0:
-        error_message = _("{} could not be unzipped.".format(installer))
-    return error_message
+        err_msg = _("{} could not be unzipped.".format(installer))
+    return err_msg
 
 
 def move_and_overwrite(game, temp_dir, target_dir):
@@ -137,7 +175,8 @@ def move_and_overwrite(game, temp_dir, target_dir):
     if game.platform == "linux":
         source_dir = os.path.join(temp_dir, "data/noarch")
     else:
-        source_dir = temp_dir
+        innoextract_dir = os.path.join(temp_dir, "minigalaxy_game_files")
+        source_dir = temp_dir if not os.path.isdir(innoextract_dir) else innoextract_dir
     for src_dir, dirs, files in os.walk(source_dir):
         destination_dir = src_dir.replace(source_dir, target_dir, 1)
         if not os.path.exists(destination_dir):
