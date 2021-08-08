2737bb2058168e03f930ce3aec2cd8b3623d480f
Tomasz Makarewicz <tomasz.makarewicz@protonmail.ch>
2021-08-08 23:56:23 +0200

    Simplyfie direcory structure

--- minigalaxy/installer.py
+++ minigalaxy/installer.py
@@ -138,18 +138,17 @@ def extract_windows(game, installer, temp_dir):
 def extract_by_innoextract(installer, temp_dir):
     err_msg = ""
     if shutil.which("innoextract"):
-        inno_files_dir = os.path.join(temp_dir, "minigalaxy_game_files")
-        cmd = ["innoextract", installer, "-d", inno_files_dir]
+        cmd = ["innoextract", installer, "-d", temp_dir]
         stdout, stderr, exitcode = _exe_cmd(cmd)
         if exitcode not in [0]:
             err_msg = _("Innoextract extraction failed.")
         else:
-            inno_app_dir = os.path.join(inno_files_dir, "app")
+            inno_app_dir = os.path.join(temp_dir, "app")
             if os.path.isdir(inno_app_dir):
-                _mv(inno_app_dir, inno_files_dir)
+                _mv(inno_app_dir, temp_dir)
             innoextract_unneeded_dirs = ["__redist", "tmp", "commonappdata", "app"]
             for unneeded_dir in innoextract_unneeded_dirs:
-                unneeded_dir_full_path = os.path.join(inno_files_dir, unneeded_dir)
+                unneeded_dir_full_path = os.path.join(temp_dir, unneeded_dir)
                 if os.path.isdir(unneeded_dir_full_path):
                     shutil.rmtree(unneeded_dir_full_path)
     else:
