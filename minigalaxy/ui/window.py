531f4c40c8180f389c5993b744394185db9e480e
TotalCaesar659 <14265316+TotalCaesar659@users.noreply.github.com>
2022-02-08 15:58:25 +0300

    Use information cover cache

--- minigalaxy/ui/window.py
+++ minigalaxy/ui/window.py
@@ -6,7 +6,7 @@ from minigalaxy.ui.preferences import Preferences
 from minigalaxy.ui.about import About
 from minigalaxy.api import Api
 from minigalaxy.config import Config
-from minigalaxy.paths import UI_DIR, LOGO_IMAGE_PATH, THUMBNAIL_DIR
+from minigalaxy.paths import UI_DIR, LOGO_IMAGE_PATH, THUMBNAIL_DIR, COVER_DIR
 from minigalaxy.translation import _
 from minigalaxy.ui.library import Library
 from minigalaxy.ui.gtk import Gtk, Gdk, GdkPixbuf
@@ -65,6 +65,10 @@ class Window(Gtk.ApplicationWindow):
         # Create the thumbnails directory
         if not os.path.exists(THUMBNAIL_DIR):
             os.makedirs(THUMBNAIL_DIR, mode=0o755)
+
+        # Create the covers directory
+        if not os.path.exists(COVER_DIR):
+            os.makedirs(COVER_DIR, mode=0o755)
 
         # Interact with the API
         self.offline = not self.api.can_connect()
