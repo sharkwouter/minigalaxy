f8ea49d320af60d485fd1811365c14db7265114b
Wouter Wijsman <wwijsman@live.nl>
2022-09-24 10:04:14 +0200

    Increase test coverage for Config and fix bug

--- tests/test_config.py
+++ tests/test_config.py
@@ -38,6 +38,7 @@ class TestConfig(TestCase):
         self.assertEqual("lang", config.lang)
         self.assertEqual("view", config.view)
         self.assertEqual("install_dir", config.install_dir)
+        self.assertEqual("username", config.username)
         self.assertEqual("refresh_token", config.refresh_token)
         self.assertEqual(True, config.keep_installers)
         self.assertEqual(False, config.stay_logged_in)
@@ -61,7 +62,6 @@ class TestConfig(TestCase):
         self.assertEqual("", config.username)
         self.assertEqual(DEFAULT_INSTALL_DIR, config.install_dir)
         self.assertEqual("", config.refresh_token)
-        self.assertEqual("", config.username)
         self.assertEqual(False, config.keep_installers)
         self.assertEqual(True, config.stay_logged_in)
         self.assertEqual(False, config.use_dark_theme)
@@ -93,7 +93,6 @@ class TestConfig(TestCase):
         self.assertEqual("", config.username)
         self.assertEqual(DEFAULT_INSTALL_DIR, config.install_dir)
         self.assertEqual("", config.refresh_token)
-        self.assertEqual("", config.username)
         self.assertEqual(False, config.keep_installers)
         self.assertEqual(True, config.stay_logged_in)
         self.assertEqual(False, config.use_dark_theme)
@@ -103,6 +102,77 @@ class TestConfig(TestCase):
         self.assertEqual(False, config.installed_filter)
         self.assertEqual(False, config.create_applications_file)
         self.assertEqual([], config.current_downloads)
+
+    @patch("os.path.isfile")
+    def test_config_property_setters(self, mock_isfile: MagicMock):
+        mock_isfile.return_value = True
+        filename = "/this/is/a/test"
+        config_data = "{}"
+        with patch("builtins.open", mock_open(read_data=config_data)):
+            config = Config(filename)
+            config._Config__write = MagicMock()
+
+        self.assertEqual("", config.locale)
+        config.locale = "en_US.UTF-8"
+        self.assertEqual("en_US.UTF-8", config.locale)
+
+        config._Config__write.assert_called_once()
+
+        self.assertEqual("en", config.lang)
+        config.lang = "pl"
+        self.assertEqual("pl", config.lang)
+
+        self.assertEqual("grid", config.view)
+        config.view = "list"
+        self.assertEqual("list", config.view)
+
+        self.assertEqual("", config.username)
+        config.username = "username"
+        self.assertEqual("username", config.username)
+
+        self.assertEqual(DEFAULT_INSTALL_DIR, config.install_dir)
+        config.install_dir = "/install/dir"
+        self.assertEqual("/install/dir", config.install_dir)
+
+        self.assertEqual("", config.refresh_token)
+        config.refresh_token = "refresh_token"
+        self.assertEqual("refresh_token", config.refresh_token)
+
+        self.assertEqual(False, config.keep_installers)
+        config.keep_installers = True
+        self.assertEqual(True, config.keep_installers)
+
+        self.assertEqual(True, config.stay_logged_in)
+        config.stay_logged_in = False
+        self.assertEqual(False, config.stay_logged_in)
+
+        self.assertEqual(False, config.use_dark_theme)
+        config.use_dark_theme = True
+        self.assertEqual(True, config.use_dark_theme)
+
+        self.assertEqual(False, config.show_hidden_games)
+        config.show_hidden_games = True
+        self.assertEqual(True, config.show_hidden_games)
+
+        self.assertEqual(False, config.show_windows_games)
+        config.show_windows_games = True
+        self.assertEqual(True, config.show_windows_games)
+
+        self.assertEqual(False, config.keep_window_maximized)
+        config.keep_window_maximized = True
+        self.assertEqual(True, config.keep_window_maximized)
+
+        self.assertEqual(False, config.installed_filter)
+        config.installed_filter = True
+        self.assertEqual(True, config.installed_filter)
+
+        self.assertEqual(False, config.create_applications_file)
+        config.create_applications_file = True
+        self.assertEqual(True, config.create_applications_file)
+
+        self.assertEqual([], config.current_downloads)
+        config.current_downloads = [1, 2, 3]
+        self.assertEqual([1, 2, 3], config.current_downloads)
 
     @patch('os.path.isfile')
     def test_get(self, mock_isfile: MagicMock):
