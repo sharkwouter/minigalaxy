ec823d4148fe9182c92c6e748228a527e45f0dcf
Wouter Wijsman <wouter@gridscale.io>
2022-10-07 13:56:03 +0200

    Use webkit 4.1 when possible

--- minigalaxy/ui/webkit.py
+++ minigalaxy/ui/webkit.py
@@ -1,4 +1,7 @@
 import gi
 
-gi.require_version('WebKit2', '4.0')
+try:
+    gi.require_version('WebKit2', '4.1')
+except ValueError:
+    gi.require_version('WebKit2', '4.0')
 from gi.repository import WebKit2  # noqa: E402,F401
