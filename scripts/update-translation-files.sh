12b6114e4949f8c97e45f5019a6522f919138097
Wouter Wijsman <wwijsman@live.nl>
2026-06-15 18:00:24 +0200

    Fix translation update script and update translation files

--- scripts/update-translation-files.sh
+++ scripts/update-translation-files.sh
@@ -4,8 +4,8 @@ cd "$(dirname "$0")"/..
 POTFILE="data/po/minigalaxy.pot"
 
 # Generate the pot file
-xgettext --from-code=UTF-8 --keyword=_ --sort-output --add-comments --language=Python minigalaxy/*.py minigalaxy/ui/*.py bin/minigalaxy -o "${POTFILE}"
-xgettext --join-existing --from-code=UTF-8 --keyword=translatable --sort-output --language=Glade data/ui/*.ui -o "${POTFILE}"
+xgettext --from-code=UTF-8 --keyword=_ --sort-output --add-comments --language=Python minigalaxy/*.py minigalaxy/ui/*.py minigalaxy/download/*.py minigalaxy/entity/*.py -o "${POTFILE}"
+xgettext --join-existing --from-code=UTF-8 --keyword=translatable --sort-output --language=Glade minigalaxy/ui/data/*.ui -o "${POTFILE}"
 
 # Update each po file
 for langfile in data/po/*.po; do
