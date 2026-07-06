8423a02e09941f2a78a420135c75227f21af8821
GB609 <39741460+GB609@users.noreply.github.com>
2026-07-06 19:01:54 +0200

    disable question for mono at prefix creation time via ENV

--- minigalaxy/installer.py
+++ minigalaxy/installer.py
@@ -235,7 +235,7 @@ def extract_by_wine(game, installer, game_lang, config=Config()):
     prefix_dir = os.path.join(game.install_dir, "prefix")
     wine_env = [
         f"WINEPREFIX={prefix_dir}",
-        "WINEDLLOVERRIDES=winemenubuilder.exe=d"
+        "WINEDLLOVERRIDES=winemenubuilder.exe=d,mscoree=d,mshtml=d"
     ]
     wine_bin = get_wine_path(game)
 
